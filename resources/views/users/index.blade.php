5270e273d8d211079a2fddefe4b7b58ebd09fdd5
kopyorsultan <cici.yora.santoso@mhs.politala.ac.id>
2023-12-26 21:32:15 +0800

    last update

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -200,7 +200,7 @@
         <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="modalCenterTitle">Tambah Data Users</h5>
+                    <h5 class="modal-title" id="modalCenterTitle">Edit Data Users</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <form method="post" action="/users" enctype="multipart/form-data">
